f63f422cf1c3ec690c32ad543aeba53671ec278b
gur79 <gur79tub@gmail.com>
2018-08-29 16:37:55 +0200

    view.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from blog.models import BlogEntries
+from blog.serializers import BlogEntriesSerializer
+
+from rest_framework import mixins
+from rest_framework import generics
+
+
 
 # Create your views here.
+
+
+
+class blogEntriesList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = BlogEntries.objects.all()
+    serializer_class = BlogEntriesSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+
+
+
+class BlogEntryDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = BlogEntries.objects.all()
+    serializer_class = BlogEntriesSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
