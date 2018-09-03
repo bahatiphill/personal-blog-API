1c8a7d067aa99c17c07d8bd97243186adb4d25ab
gur79 <gur79tub@gmail.com>
2018-09-03 15:10:23 +0200

    timestamp read_only serialization

--- blog/serializers.py
+++ blog/serializers.py
@@ -7,8 +7,9 @@ from blog.models import BlogEntries
 class BlogEntriesSerializer(serializers.ModelSerializer):
 
     slug = serializers.SlugField(read_only=True)
+    timestamp = serializers.DateField(read_only=True)
 
     class Meta:
         model = BlogEntries
-        fields = ('id','title','body', 'tags', 'slug', 'published')
+        fields = ('id','title','body', 'tags', 'slug', 'published', 'timestamp')
         #read_only_fields = ('slug',)
